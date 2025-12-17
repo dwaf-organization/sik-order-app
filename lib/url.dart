2b06a8a88910209d5a28e5c3f38c8d09c92bb338
donguk585 <137376258+donguk585@users.noreply.github.com>
2025-12-17 19:12:29 +0900

    Update url.dart

--- lib/url.dart
+++ lib/url.dart
@@ -1,7 +1,7 @@
 class UrlConfig {
   static const String localServer = "http://10.0.2.2:8000";
   static const String chromeServer = "http://localhost:8000";
-  static const String testphone = "http://127.0.0.1:8000";
+  static const String testphone = "https://api.sikorder.shop";
   static const String awsServer = "https://api.sikorder.shop";
   static const String server = "";
   static const String serverUrl = awsServer;
