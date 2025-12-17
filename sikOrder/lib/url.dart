4b6defce2003c3d5aeebdc972cdf9c214e8c284d
donguk585 <137376258+donguk585@users.noreply.github.com>
2025-12-17 16:28:55 +0900

    3

--- sikOrder/lib/url.dart
+++
@@ -1,8 +0,0 @@
-class UrlConfig {
-  static const String localServer = "http://10.0.2.2:8000";
-  static const String chromeServer = "http://localhost:8000";
-  static const String testphone = "http://127.0.0.1:8000";
-  static const String awsServer = "http://api.sikorder.shop:8000";
-  static const String server = "";
-  static const String serverUrl = localServer;
-}
